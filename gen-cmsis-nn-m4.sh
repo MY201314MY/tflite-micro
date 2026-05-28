0e119417df0b8d81b72fbe93548dcc4c4084ea9c
pi <3477669308@qq.com>
2026-05-28 23:39:52 +0800

    cnn accelerate.

---
+++ gen-cmsis-nn-m4.sh
@@ -0,0 +1,3 @@
+python3 tensorflow/lite/micro/tools/project_generation/create_tflm_tree.py \
+    --makefile_options="OPTIMIZED_KERNEL_DIR=cmsis_nn TARGET_ARCH=TARGET_ARCH=cortex-m4" \
+    /opt/cmsis_nn_m4
